ef04b761ae4c8480278ac1d48cb032246692d7c9
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2018-07-05 15:55:38 +0200

    BIG CHANGE : only one python file

--- github_reader.py
+++ github_reader.py
@@ -7,21 +7,7 @@ from ictv.plugin_manager.plugin_utils import MisconfiguredParameters
 import json
 import urllib.request
 from ictv.plugins.github_reader.githubApp import GitIctv
-
-def parse_stat(stat):
-    return None
-
-def parse_issues(issues):
-    return None
-
-def pasre_commits(commits):
-    return None
-
-def parse_releases(releases):
-    return None
-
-def parse_contributors(contributors):
-    return None
+from github import Github
 
 def get_content(channel_id):
     print("get_content")
@@ -52,54 +38,32 @@ def get_content(channel_id):
         return []
     print("After token")
 
-    git_obj = GitIctv(token,repo_url,orga_url)
+    git_obj = Github(token)
+    capsule = GithubReaderCapsule()
 
     print("After init gitIctv")
-
-    #Test
-    stat = None
-    issue_list = None
-    commit_list = None
-    release_list = None
-    contributor_list = None
-    organization_list = None
 
     #if disp_stat:
     #    stat = git_obj.get_stat()
     if disp_issues:
-        issue_list = git_obj.get_issue(number_issues)
+        capsule._slides.append(GithubReaderSlideIssue(repo_url, number_issues, duration, git_obj))
     if disp_commits:
-        commit_list = git_obj.get_commit(number_commits)
+        capsule._slides.append(GithubReaderSlideCommit(repo_url, number_commits, duration, git_obj))
     if disp_releases:
         release_list = git_obj.get_release()
     #if disp_contributors:
     #    contributor_list = git_obj.get_contributor()
     if had_organization:
-        organization_list = git_obj.get_organization(number_organizations)
+        capsule._slides.append(GithubReaderSlideOrganization(orga_url, number_organizations, duration, git_obj))
 
     print("BeforeReturn")
 
-    return [GithubReaderCapsule(repo_url,stat,issue_list,commit_list,release_list,contributor_list,organization_list,duration)]
+    return [capsule]
 
 class GithubReaderCapsule(PluginCapsule):
 
-    def __init__(self,repo_url,stat,issue_list,commit_list,release_list,contributor_list,organization_list,duration):
-        print("GithubReaderCapsule")
-        self._slides = []
-        print(type(issue_list))
-        if stat:
-            self._slides.append(GithubReaderSlide(stat,duration,'stat'))
-        if issue_list:
-            print("GithubReaderCapsule : issue_list")
-            self._slides.append(GithubReaderSlide(repo_url,issue_list,duration,'issue'))
-        if commit_list:
-            self._slides.append(GithubReaderSlide(repo_url,commit_list,duration,'commit'))
-        if release_list:
-            self._slides.append(GithubReaderSlide(repo_url,release_list,duration,'release'))
-        if contributor_list:
-            self._slides.append(GithubReaderSlide(repo_url,contributor_list,duration,'contributor'))
-        if organization_list:
-            self._slides.append(GithubReaderSlide(repo_url,organization_list,duration,'organization'))
+    def __init__(self):
+    		self._slides = []
 
     def get_slides(self):
         return self._slides
@@ -111,65 +75,8 @@ class GithubReaderCapsule(PluginCapsule):
         return str(self.__dict__)
 
 class GithubReaderSlide(PluginSlide):
-    def __init__(self,repo_url,list, duration,mark = None):
-        print("GithubReaderSlide")
-        self._content = {}
-        #self._content['title-1'] = {'text':repo_url.split('/')[1]}
-        self._content['title-1'] = {'text': 'title'}
-        self._content['subtitle-1'] = {'text': 'subtitle'}
-        self._duration = duration
-        i = 1
-        if mark == 'organization':
-            self._content['title-1'] = {'text': 'Github activity'}
-            self._content['subtitle-1'] = {'text': list['name']}
-            print(mark)
-            dispText = ''
-            print('before dispText')
-            for repo in list['repos']:
-                dispText += repo+'<br>'
-                print(dispText)
-                i += 1
-            self._content['text-'+str(1)] = {'text': dispText}
-            self._content['image-'+str(1)] = {'src': list['avatar-url']}
-            #for repo in list['repos']:
-            #    self._content['text-'+str(i)] = {'text':list['name']+"<br>"+repo}
-            #    self._content['image-'+str(i)] = {'src':list['avatar-url']}
-            #    i += 1
-        else:
-            for elem in list:
-                #if mark == 'stat': #TODO
-                #    self._content['text-'+str(i)] = elem[message]
-                #    self._content['image-'+str(i)] = elem[text]
-                print('text-'+str(i))
-                print(mark)
-                if mark == 'issue':
-                    if elem['state'] == 'open':
-                        self._content['text-'+str(i)] = {'text': elem['title']+"<br>state : "+"<font color = \"green\">"+elem['state']+"</font>"+"<br>"+elem['comments']}
-                    elif elem['state'] == 'closed':
-                        self._content['text-'+str(i)] = {'text': elem['title']+"<br>state : "+"<font color = \"red\">"+"closed at : "+"</font>"+elem['closed_at']+"<br>"+elem['comments']}
-                    #else TODO, undefined ?
-                    self._content['image-'+str(i)] = {'src': elem['avatar_url']}
-                elif mark == 'commit':
-                    print("commit")
-                    print("it : "+str(i))
-                    print(elem['message'])
-                    print(elem['author'])
-                    print(elem['created_at'])
-                    print(elem['avatar_url'])
-                    self._content['text-'+str(i)] = {'text': elem['author']+"<br>created at : "+elem['created_at']+"<br>"+elem['message']}
-                    self._content['image-'+str(i)] = {'src': elem['avatar_url']}
-                elif mark == 'release':
-                    self._content['text-'+str(i)] = {'text':elem['title']+"<br>author :"+elem['author']+ ", created at : "+elem['created_at']+", version :"+elem['version']+"<br>"+elem['body']}
-                    self._content['image-'+str(i)] = ""
-                #elif mark == 'contributor': #TODO
-                #    self._content['text-'+str(i)] = elem[message]
-                #    self._content['image-'+str(i)] = elem[text]
-                else:
-                    break #raise an error unknown mark (or None mark)
-                i += 1
-        self._content['background-1']={'src': 'plugins/github_reader/github-background.png', 'color': 'black', 'size': 'content'}
-
-
+    def __init__(self):
+        pass
     def get_duration(self):
         return self._duration
 
@@ -182,8 +89,107 @@ class GithubReaderSlide(PluginSlide):
     def __repr__(self):
         return str(self.__dict__)
 
-#if __name__ == "__main__":
-#
-#    git = GitIctv("TOKEN", 'fdardenne/TestRepo', "scala")
-#    t = git.get_organization()
-#    print(t)
+class GithubReaderSlideIssue(GithubReaderSlide):
+    def __init__(self, repo_url, nb_elem,duration,git_obj):
+
+        self._content = {}
+        self._content['title-1'] = {'text':repo_url.split('/')[1]}
+        self._content['subtitle-1'] = {'text': 'Last modified issues'}
+        self._duration = duration
+
+        issues = git_obj.get_repo(repo_url).get_issues(state="all")
+        for i,issue in enumerate(issues[:nb_elem]):
+            if issue.state == 'open':
+                self._content['text-'+str(i+1)] = {'text': issue.title+"<br>"+"<font color = \"#7FFF00\">"+"opened"+"</font>"+" on "+issue.created_at.strftime("%d %B %Y %H:%M")+"<br>"+"# comments : "+str(issue.comments)}
+            elif issue.state == 'closed':
+              	self._content['text-'+str(i+1)] = {'text': issue.title+"<br>"+"<font color = \"red\">"+"closed"+"</font>"+" on "+issue.closed_at.strftime("%d %B %Y %H:%M")+"<br>"+"# comments : "+str(issue.comments)}
+            else:
+              	try:
+                    logger.warning('None state issue'+issue.title, extra=logger_extra)
+              	except Exception as e:
+                    logger.warning('None state issue', extra=logger_extra)
+            self._content['image-'+str(i+1)] = {'src': issue.user.avatar_url}
+        self._content['background-1']={'src': 'plugins/github_reader/github-background.png', 'color': 'black', 'size': 'content'}
+
+#=====================================FLO
+class GithubReaderSlideCommit(GithubReaderSlide):
+    def __init__(self, repo_url, number_of_commits, duration, git_obj):
+        repo = git_obj.get_repo(repo_url)
+        commits = repo.get_commits()
+        commit_list = []
+        for commit in commits[:number_of_commits]:
+            message = commit.commit.message
+            message = message.split("\n")[0]
+            name = commit.author.name
+            if(not name):
+                name = "Undefined"
+            commit_list.append({'author': name, 'message': message, "created_at": commit.commit.author.date.strftime("%d %B %Y %H:%M"), 'avatar_url':commit.author.avatar_url})
+        	#TODO SEE IF MORE THAN 0 COMMITS
+        self._content = {}
+        self._content['title-1'] = {'text':repo_url.split('/')[1]}
+        self._content['subtitle-1'] = {'text': "Commits"}
+        self._duration = duration
+        i = 1
+        for elem in commit_list:
+            print("commit")
+            print("it : "+str(i))
+            print(elem['message'])
+            print(elem['author'])
+            print(elem['created_at'])
+            print(elem['avatar_url'])
+            self._content['text-'+str(i)] = {'text': elem['author']+"<br>created at : "+elem['created_at']+"<br>"+elem['message']}
+            self._content['image-'+str(i)] = {'src': elem['avatar_url']}
+            i += 1
+        self._content['background-1']={'src': 'plugins/github_reader/github-background.png', 'color': 'black', 'size': 'content'}
+
+
+#=====================================END FLO
+
+
+
+
+class GithubReaderSlideRelease():
+  pass
+class GithubReaderSlideContributor():
+  pass
+class GithubReaderSlideOrganization(GithubReaderSlide):
+    def __init__(self, orga_url, number_organizations, duration, git_obj):
+        #git_obj = Github(token)
+        organization = git_obj.get_organization(orga_url)
+        # et si organization n'existe pas ???? TODO
+
+        repos_organization = []
+
+        repos = [e for e in organization.get_repos()] #easier to print repos, makes a list from a Paginated list(which has not a suitable print function)
+
+        print(repos)
+        sorted_repos = sorted(repos, reverse=True, key=lambda k: k.updated_at)
+        print(sorted_repos)
+        for repo in sorted_repos[:number_organizations]:
+        #for count in range(number_organizations):
+        #    repo = sorted_repos[count]
+            print(repo,repo.updated_at)
+            print(repo.full_name.split('/')[1])
+            # FORMAT TODO
+            repos_organization.append('<B>'+repo.full_name.split('/')[1]+'</B>'+' updated at : ' + repo.updated_at.strftime("%d %B %Y %H:%M")) #TODO if datetime = vide ??
+
+        dico = {"avatar-url": organization.avatar_url, "name": organization.name, "repos":repos_organization}
+
+        self._content = {}
+        self._content['title-1'] = {'text': 'Last modified repos'}
+        self._content['subtitle-1'] = {'text': organization.name}
+        self._duration = duration
+        print('organization')
+        dispText = ''
+        print('before dispText')
+        i = 1
+        for repo in repos_organization:
+            dispText += repo+'<br>'
+            print(dispText)
+            i += 1
+        self._content['text-'+str(1)] = {'text': dispText}
+        self._content['image-'+str(1)] = {'src': organization.avatar_url}
+        self._content['background-1']={'src': 'plugins/github_reader/github-background.png', 'color': 'black', 'size': 'content'}
+
+class GithubReaderSlideStat():
+  pass
