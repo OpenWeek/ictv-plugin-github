757383bc162f194eabbfbfce69dc29d6925e01a5
Flo <dardenneflorent@outlook.com>
2018-07-05 20:22:01 +0200

    Added Authors, GNU Affero General Public License

--- github_reader.py
+++ github_reader.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+#    Written by Dardenne Florent, Fiset Alexandre, Gobeaux Alexandre
+#
+#    ICTV github-reader is free plugin software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ICTV github-reader is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with ICTV github-reader.  If not, see <http://www.gnu.org/licenses/>.
+
 from pyquery import PyQuery
 from ictv.models.channel import PluginChannel
 from ictv.plugin_manager.plugin_capsule import PluginCapsule
